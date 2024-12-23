bc61e26958508b8282bfe7fb927baecaa15f8710
Alex Ponciano <73028034+alexponciano2@users.noreply.github.com>
2024-12-22 22:16:55 -0300

    MENU HANBURGUER

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,28 +1,50 @@
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
-import logo from './assets/logo2.png'
-import styles from './index.module.scss'
+import logo from './assets/logo2.png';
+import styles from './index.module.scss';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 
 function NavbarHome() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
-        <>
-            <nav className={styles.s_navbar}>
-                <Container>
-                    <div className={styles.navbar}>
-                        <div className={styles.imgDiv}>
-                            <img data-aos="fade-right" data-aos-duration="1000" data-aos-delay="50" className={styles.imgDiv__img} src={logo} alt="" />
-                        </div>
-                        <div className={styles.links}>
-                            <a data-aos="fade-right" data-aos-duration="500" data-aos-delay="350" href="#home" className={styles.items}>inicio</a>
-                            <a data-aos="fade-right" data-aos-duration="500" data-aos-delay="650" href="#infos" className={styles.items}>atuações</a>
-                            <a data-aos="fade-right" data-aos-duration="500" data-aos-delay="950" href="#servicos" className={styles.items}>serviços</a>
-                            <a data-aos="fade-right" data-aos-duration="500" data-aos-delay="1250" href="#sobre" className={styles.items}>sobre</a>
-                            <a data-aos="fade-right" data-aos-duration="500" data-aos-delay="1550" href="#postagens" className={styles.items}>postagens</a>
-                            <a data-aos="fade-right" data-aos-duration="500" data-aos-delay="1850" href="#contato" className={styles.items}>contato</a>
-                        </div>
+        <nav className={styles.s_navbar}>
+            <Container>
+                <div className={styles.navbar}>
+                    <div className={styles.imgDiv}>
+                        <img
+                            data-aos="fade-right"
+                            data-aos-duration="1000"
+                            data-aos-delay="50"
+                            className={styles.imgDiv__img}
+                            src={logo}
+                            alt="Logo"
+                        />
                     </div>
-                </Container>
-            </nav>
-        </>
+                    <div className={styles.hamburger} onClick={toggleMenu}>
+                        {menuOpen ? (
+                            <AiOutlineClose size={30} color="white" />
+                        ) : (
+                            <AiOutlineMenu size={30} color="white" />
+                        )}
+                    </div>
+                    <div
+                        className={`${styles.links} ${menuOpen ? styles.linksActive : ''}`}
+                    >
+                        <a href="#home" className={styles.items}>inicio</a>
+                        <a href="#infos" className={styles.items}>atuações</a>
+                        <a href="#servicos" className={styles.items}>serviços</a>
+                        <a href="#sobre" className={styles.items}>sobre</a>
+                        <a href="#postagens" className={styles.items}>postagens</a>
+                        <a href="#contato" className={styles.items}>contato</a>
+                    </div>
+                </div>
+            </Container>
+        </nav>
     );
 }
 
