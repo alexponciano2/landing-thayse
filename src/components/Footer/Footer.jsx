d5d03197424cb73e15acc6513ab7fe72d8b24634
Alex Ponciano <73028034+alexponciano2@users.noreply.github.com>
2024-12-30 18:07:16 -0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,8 +12,8 @@ const Footer = () => {
                         Todos os Direitos Reservados &copy; 2024 Carolina Reis
                     </small>
                     <small>Desenvolvido por Alex Ponciano ❤️ |
-                        <a href="https://linkedin.com/in/nickolas-desenvolvedor" className="ms-1 text-decoration-none">LinkedIn</a>  &
-                        <a href="https://github.com/NickolasAbad" className="ms-1 text-decoration-none">GitHub</a></small>
+                        <a href="https://linkedin.com/" className="ms-1 text-decoration-none">LinkedIn</a>  &
+                        <a href="https://github.com/" className="ms-1 text-decoration-none">GitHub</a></small>
                 </div>
             </Container>
         </footer >
